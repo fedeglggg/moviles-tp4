1a7eb910c09f87e66f91ae679bfb51e32661401f
Niem <niemil9326@gmail.com>
2019-05-13 15:45:57 -0300

    13-5-19

---
+++ app/src/main/java/com/example/tp4/Piramide.java
@@ -0,0 +1,13 @@
+package com.example.tp4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Piramide extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_piramide);
+    }
+}
