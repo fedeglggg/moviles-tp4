0560e30c40405cd9acc1204e4e06a7bc9d24ceb9
Niem <niemil9326@gmail.com>
2019-05-19 06:41:30 -0300

    piramide lista

--- app/src/main/java/com/example/tp4/Piramide.java
+++ app/src/main/java/com/example/tp4/Piramide.java
@@ -1,28 +1,111 @@
 package com.example.tp4;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.support.design.widget.CoordinatorLayout;
+
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
 import android.widget.TextView;
 
 public class Piramide extends AppCompatActivity {
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_piramide);
-        
 
 
+        // snackbar1
+        TextView textView1 = (TextView) findViewById(R.id.textView);
+        textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View view = findViewById(R.id.piramide);
+                String message = "El pollo pertenece a la categoría de Carnes y Frutos secos";
+                int duration = Snackbar.LENGTH_SHORT;
+
+                showSnackbar(view, message, duration);
+            }
+
+        });
+
+        // snackbar2
+        TextView textView2 = (TextView) findViewById(R.id.textView2);
+        textView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View view = findViewById(R.id.piramide);
+                String message = "Las galletitas pertenecen a la categoría de Aceites y Dulces";
+                int duration = Snackbar.LENGTH_SHORT;
+
+                showSnackbar(view, message, duration);
+            }
+
+        });
+
+
+
+        // snackbar3
+        TextView textView3 = (TextView) findViewById(R.id.textView3);
+        textView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View view = findViewById(R.id.piramide);
+                String message = "El arroz pertenece a la categoría de Cereales";
+                int duration = Snackbar.LENGTH_SHORT;
+
+                showSnackbar(view, message, duration);
+            }
+
+        });
+
+        // snackbar4
+        TextView textView4 = (TextView) findViewById(R.id.textView4);
+        textView4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View view = findViewById(R.id.piramide);
+                String message = "La espinaca pertenece a la categoría de Vegetales";
+                int duration = Snackbar.LENGTH_SHORT;
+
+                showSnackbar(view, message, duration);
+            }
+
+        });
+
+        // snackbar5
+        TextView textView5 = (TextView) findViewById(R.id.textView5);
+        textView5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View view = findViewById(R.id.piramide);
+                String message = "El tomate pertenece a la categoría de Frutas";
+                int duration = Snackbar.LENGTH_SHORT;
+
+                showSnackbar(view, message, duration);
+            }
+
+        });
+
+        // snackbar6
+        TextView textView6 = (TextView) findViewById(R.id.textView6);
+        textView6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View view = findViewById(R.id.piramide);
+                String message = "La Leche pertenece a la categoría de Lacteos";
+                int duration = Snackbar.LENGTH_SHORT;
+
+                showSnackbar(view, message, duration);
+            }
+
+        });
+    }   // oncreate
+
+
+    public void showSnackbar(View view, String message, int duration)
+    {
+        Snackbar.make(view, message, duration).show();
     }
 }
